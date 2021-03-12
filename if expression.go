abfa601a6531ae7b6b41b9c2f9ffb62186ecac25
Muhammad Tegar Akmal <eategar@gmail.com>
2021-03-12 22:49:10 +0700

    if expression

---
+++ if expression.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Tegar"
+	if name == "Tegar" {
+		fmt.Println("benar")
+	} else if name == "Akmal" {
+		fmt.Println("hmm hampir")
+	} else {
+		fmt.Println("salah")
+	}
+}
