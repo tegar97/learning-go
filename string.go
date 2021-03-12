7941d8b686c4a71bbec9b91cd8250c4a07301915
Muhammad Tegar Akmal <eategar@gmail.com>
2021-03-12 15:31:46 +0700

    string

--- string.go
+++ string.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+
+// variable adalah tempat untuk penyimpanan data
+// variabel digunakan agar kita bisa mengakses data yang sama dimanapun kita mau
+// Di Go lang variable hanya bisa menyimpan tipe data yang sama , jika kita ingin menyimpan data yang berbeda
+// -beda jenis.kita harus membuat beberapa variabel
+// Untuk membuat variabel, kita bisa menggunakan kata kunci var,lalu diikuti dengan nama variabel dan tipe datanya
+
 func main() {
-	fmt.Println(len("tegavr"))
-	fmt.Println("Eegar akmavl"[0])
+	var name string
+	name = "tegar"
+	fmt.Println(name)
+	fmt.Println(len(name))
+
+	var umur int8
+	umur  = 17
+	fmt.Println(umur)
 }
